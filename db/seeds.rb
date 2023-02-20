1d4388e4a79e110eebf6f4efcdc704779837b2ea
Gor <7495500@gmail.com>
2023-02-20 22:33:43 +0300

    Modify posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
     title:       "title #{i}",
     created_at:  Time.now - i.days,
     updated_at:  Time.now - i.days,
-    views:       rand(15..100)
+    views:       rand(15..100) / (i * 0.1)
   )
 end
